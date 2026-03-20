60540e3bd4637c10b572503d0e778c910b50439f
bonniX47 <gbonnie545@gmail.com>
2026-03-20 18:55:51 -0300

    peças

---
+++ Aula03/empresario/Program.cs
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int numeroFuncionario;
+    double horasTrabalhadas, valorPorHora;
+
+    cout << "Digite o numero do funcionario: ";
+    cin >> numeroFuncionario;
+
+    cout << "Digite as horas trabalhadas: ";
+    cin >> horasTrabalhadas;
+
+    cout << "Digite o valor por hora: ";
+    cin >> valorPorHora;
+
+    double salario = horasTrabalhadas * valorPorHora;
+
+    cout << fixed << setprecision(2);
+    cout << "NUMERO = " << numeroFuncionario << endl;
+    cout << "SALARIO = R$ " << salario << endl;
+
+    return 0;
+}
---
+++ Aula03/peças/Program.cs
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int cod1, cod2;
+    int qtd1, qtd2;
+    double valor1, valor2;
+
+    cout << "Digite o codigo da peca 1, quantidade e valor unitario: ";
+    cin >> cod1 >> qtd1 >> valor1;
+
+    cout << "Digite o codigo da peca 2, quantidade e valor unitario: ";
+    cin >> cod2 >> qtd2 >> valor2;
+
+    double total = (qtd1 * valor1) + (qtd2 * valor2);
+
+    cout << fixed << setprecision(2);
+    cout << "VALOR A PAGAR = R$ " << total << endl;
+
+    return 0;
+}
