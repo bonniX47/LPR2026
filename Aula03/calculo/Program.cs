8c7c68e18408e3db6c5ebc5000a608fc8e2011f9
bonniX47 <gbonnie545@gmail.com>
2026-03-20 18:53:24 -0300

    aula03

---
+++ Aula03/calculo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor de A: ");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de B: ");
+        int B = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de C: ");
+        int C = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de D: ");
+        int D = int.Parse(Console.ReadLine());
+
+        int diferenca = (A * B) - (C * D);
+
+        Console.WriteLine("A diferença é de: " + diferenca);
+    }
+}
