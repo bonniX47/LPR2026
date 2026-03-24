3e6b640146fc1545b6f68891b96764b96b795387
bonniX47 <gbonnie545@gmail.com>
2026-03-24 14:01:44 -0300

    ADIVINHA

---
+++ Aula05/ADIVINHAÇAO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um número entre 1 e 100:");
+        Random random = new Random();
+        int numero = random.Next(1, 101);    
+        int tentativas = 0;
+        bool acertou = false; 
+        Console.WriteLine("Tente adivinhar o número secreto!"); 
+        while (!acertou)
+        {
+            Console.Write("Digite seu palpite: ");
+            int chute = int.Parse(Console.ReadLine());
+            tentativas++;
+            if (chute == numero)
+            {
+                acertou = true;
+                Console.WriteLine($"Parabéns! Você acertou o número em {tentativas} tentativas.");
+            }
+            else if (chute < numero)
+                Console.WriteLine("O número secreto é maior. Tente novamente!");
+            
+        
+            else
+            {
+                Console.WriteLine("O número secreto é menor. Tente novamente!");
+            }
+        }
+    }
+}
