1c51f32160e7d2574c40d02839d04985a71fcd52
bonniX47 <gbonnie545@gmail.com>
2026-03-24 09:29:03 -0300

    MEDIAARITMETICA

---
+++ Aula05/MEDIA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    
+    Console.WriteLine("Digite os numeros separados por virgula:");
+    string input = Console.ReadLine();
+    int[]   numeros = Array.ConvertAll(input.Split(','), int.Parse);
+
+    var par = numeros.Where(x => x % 2 == 0);
+    if (par.Any())
+        {
+            double mediaPar = par.Average();
+            Console.WriteLine($"A média dos números pares é: {mediaPar}");
+        }
+        else
+        {
+            Console.WriteLine("Não há números pares na lista.");
+        }
+
+     
+        
+    }
+}
