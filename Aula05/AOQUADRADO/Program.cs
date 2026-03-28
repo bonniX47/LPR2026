f58f3006a7c1cf397a6e6457b78dcfa0eeec933c
bonniX47 <gbonnie545@gmail.com>
2026-03-28 19:43:38 -0300

    SDM3

---
+++ Aula05/AOQUADRADO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um número para descobrir seu quadrado: ");
+        int numero = int.Parse(Console.ReadLine());
+        int quadrado = numero * numero;
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"O quadrado de {i} é {i * i}");
+        }
+        Console.WriteLine($"O quadrado de {numero} é {quadrado}");
+    }
+}
---
+++ Aula05/SDM3/Program.cs
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace SDM3;
+
+int main() {
+    int soma = 0;
+
+    for (int i = 50; i <= 500; i++) {
+        if (i % 2 != 0 && i % 3 == 0) {
+            soma += i;
+        }
+    }
+
+    cout << soma;
+    return 0;
+}
